ca868d55bef54cfb9fdae00dea145a2380a00645
JudahSan <baraka.judah@yahoo.com>
2024-07-19 13:39:45 +0300

    Create game scene

--- src/kaboomLoader.js
+++ src/kaboomLoader.js
@@ -1,1 +1,14 @@
 import kaboom from "../lib/kaboom.mjs";
+
+export const scale = 2;
+
+// Store kaboom context in a constant
+export const k = kaboom({
+  width: 640 * scale,
+  height: 320 * scale,
+  scale,
+  global: false,
+  letterbox: true,
+  debug: true,
+  crisp: true,
+});
---
+++ src/main.js
@@ -0,0 +1,1 @@
+import { k } from "./kaboomLoader.js"; // Specify extension. Using js modules instead of npm based workflow
